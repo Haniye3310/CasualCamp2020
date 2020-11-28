c17cacb389591b4ba44490a9edc7ab77ead67379
e-fo <ehsanfoadian@gmail.com>
2020-11-28 21:12:02 +0330

    - Implemented soldier select panel (toggle icons feature)

---
+++ Assets/Scripts/UI/SoldierIcon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SoldierIcon : MonoBehaviour
+{
+    public SoldierType SoldierType;
+
+    [SerializeField] Sprite _selectedIcon;
+
+    Image _image;
+    Sprite _defaultIcon;
+    StartGamePanel _startGamePanel;
+
+    void Start() 
+    {
+        _image = gameObject.GetComponent<Image>();
+        _defaultIcon = _image.sprite;
+        _startGamePanel = gameObject.GetComponentInParent<StartGamePanel>();
+        _startGamePanel.RegisterIcon(SoldierType, this);
+    }
+
+    public void SetSelected(bool isSelected) 
+    {
+        if(isSelected) 
+        {
+            _image.sprite = _selectedIcon;
+        }
+        else 
+        {
+            _image.sprite = _defaultIcon;
+        }
+    }
+
+    public void SetSelected_OnClick() {
+        _startGamePanel.SetSelectedSoldier(SoldierType);
+    }
+}
---
+++ Assets/Scripts/UI/StartGamePanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum SoldierType {
+    EN, FR, IT
+}
+
+public class StartGamePanel : MonoBehaviour
+{
+    Dictionary<SoldierType, SoldierIcon> _iconsDict = new Dictionary<SoldierType, SoldierIcon>();
+
+    public void RegisterIcon(SoldierType type, SoldierIcon icon) {
+        _iconsDict.Add(type, icon);
+    }
+
+    public void SetSelectedSoldier(SoldierType soldierType) 
+    {
+        foreach(SoldierIcon icon in _iconsDict.Values) 
+        {
+            icon.SetSelected(false);
+        }
+        _iconsDict[soldierType].SetSelected(true);
+    }
+}
